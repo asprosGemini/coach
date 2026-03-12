86972956542c77841ac02c3cbd89c5f36ce05164
Frank <rigo.noyola@hotmail.com>
2026-03-12 15:56:17 -0700

    Initial Coach app project

---
+++ coach/coachApp.swift
@@ -0,0 +1,17 @@
+//
+//  coachApp.swift
+//  coach
+//
+//  Created by Frank Noyola on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct coachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
